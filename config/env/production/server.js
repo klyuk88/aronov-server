c60d9b5321225cd3a660b7a1d97e2e66129f3d63
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2023-04-02 14:29:12 +0300

    fix api url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: 1337,
-  url: 'http://api.aronovlaw.ru/',
+  url: 'https://api.aronovlaw.ru/',
   app: {
     keys: env.array('APP_KEYS'),
   },
